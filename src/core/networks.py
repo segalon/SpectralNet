f48dc549c5679734b41d3da7364859d304eed0f9
Daniel Segal <dsthuria@gmail.com>
2022-01-14 20:01:18 +0200

    Update networks.py

--- src/core/networks.py
+++ src/core/networks.py
@@ -133,12 +133,12 @@ class SpectralNet:
 
         # create the train step update
         self.learning_rate = tf.Variable(0., name='spectral_net_learning_rate')
-        tf.initialize_all_variables().run()
+        tf.global_variables_initializer()
 
         self.train_step = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate).minimize(self.loss, var_list=self.net.trainable_weights)
 
         # initialize spectralnet variables
-        tf.initialize_all_variables().run()
+        tf.global_variables_initializer()
 
         K.get_session().run(tf.variables_initializer(self.net.trainable_weights))
 
